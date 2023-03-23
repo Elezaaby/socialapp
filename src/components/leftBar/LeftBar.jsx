0c990e0099523aefb7a3700ac05c62fbd31cf513
Elezaaby <mahmodalezaby@gmail.com>
2023-03-24 01:49:53 +0200

    add components  leftbar and navbar design

--- src/components/leftBar/LeftBar.jsx
+++ src/components/leftBar/LeftBar.jsx
@@ -1,9 +1,96 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './leftBar.scss'
+import { AuthContext } from './../../context/authContext';
+
+
+import Friends from "../../assets/friend.png";
+import Groups from "../../assets/groups.png";
+import Market from "../../assets/marketplace.png";
+import Watch from "../../assets/watch.png";
+import Memories from "../../assets/memories.png";
+import Events from "../../assets/events.png";
+import Gaming from "../../assets/gaming.png";
+import Gallery from "../../assets/gallery.png";
+import Videos from "../../assets/videos.png";
+import Messages from "../../assets/messages.png";
+import Tutorials from "../../assets/tutorials.png";
+import Courses from "../../assets/courses.png";
+import Fund from "../../assets/fundraiser.png";
 
 const LeftBar = () => {
+  const { userData } = useContext(AuthContext)
+
   return (
-    <div className='leftBar'>LeftBar</div>
+    <div className='leftBar'>
+      <div className="container">
+        <div className="menu">
+          <div className="user_info">
+            <img src={userData.profileImg} alt="" />
+            <span>{userData.name}</span>
+          </div>
+          <div className="item">
+            <img src={Friends} alt="" />
+            <span>Friends</span>
+          </div>
+          <div className="item">
+            <img src={Groups} alt="" />
+            <span>Groups</span>
+          </div>
+          <div className="item">
+            <img src={Market} alt="" />
+            <span>Marketplace</span>
+          </div>
+          <div className="item">
+            <img src={Watch} alt="" />
+            <span>Watch</span>
+          </div>
+          <div className="item">
+            <img src={Memories} alt="" />
+            <span>Memories</span>
+          </div>
+        </div>
+        <hr />
+        <div className="menu">
+          <span>Your shortcuts</span>
+          <div className="item">
+            <img src={Events} alt="" />
+            <span>Events</span>
+          </div>
+          <div className="item">
+            <img src={Gaming} alt="" />
+            <span>Gaming</span>
+          </div>
+          <div className="item">
+            <img src={Gallery} alt="" />
+            <span>Gallery</span>
+          </div>
+          <div className="item">
+            <img src={Videos} alt="" />
+            <span>Videos</span>
+          </div>
+          <div className="item">
+            <img src={Messages} alt="" />
+            <span>Messages</span>
+          </div>
+        </div>
+        <hr />
+        <div className="menu">
+          <span>Others</span>
+          <div className="item">
+            <img src={Fund} alt="" />
+            <span>Fundraiser</span>
+          </div>
+          <div className="item">
+            <img src={Tutorials} alt="" />
+            <span>Tutorials</span>
+          </div>
+          <div className="item">
+            <img src={Courses} alt="" />
+            <span>Courses</span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './navbar.scss'
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import { Link } from 'react-router-dom';
+import { AuthContext } from './../../context/authContext';
+import { ModeContext } from '../../context/modeContext';
+
 
 const Navbar = () => {
+
+  const { userData } = useContext(AuthContext)
+  const { toggle, pageMode } = useContext(ModeContext)
+
+
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <div className="left">
+        <Link to="/socialapp/home" style={{ textDecoration: "none" }}>
+          <span>SocialApp</span>
+        </Link>
+        <HomeOutlinedIcon />
+        {pageMode ? (
+          <WbSunnyOutlinedIcon onClick={toggle} />
+        ) : (
+          <DarkModeOutlinedIcon onClick={toggle} />
+        )}
+        <GridViewOutlinedIcon />
+        <div className="search">
+          <SearchOutlinedIcon />
+          <input type='text' placeholder='search' />
+        </div>
+      </div>
+      <div className="right">
+        <PersonOutlinedIcon />
+        <EmailOutlinedIcon />
+        <NotificationsOutlinedIcon />
+        <div className="user_info">
+          <img src={userData.profileImg} alt="" />
+          <span>{userData.name}</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
