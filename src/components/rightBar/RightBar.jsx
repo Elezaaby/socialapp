515be945fd4bf7b1eb930c62fc4ae3be4850aa1a
Elezaaby <mahmodalezaby@gmail.com>
2023-03-25 00:15:12 +0200

    add components rightbar design

--- src/components/rightBar/RightBar.jsx
+++ src/components/rightBar/RightBar.jsx
@@ -3,7 +3,210 @@ import './rightBar.scss'
 
 const RightBar = () => {
   return (
-    <div>RightBar</div>
+    <div className='rightBar'>
+      <div className="container">
+        <div className="menu">
+          <span>Suggestions For You</span>
+          <div className="user">
+            <div className="user_info">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <span>Adisdc md</span>
+            </div>
+            <div className="btn">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="user_info">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <span>Adisdc md</span>
+            </div>
+            <div className="btn">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+        </div>
+        <div className="menu">
+          <span>Latest Activities</span>
+          <div className="user">
+            <div className="user_info">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                <span>Adisdc md </span>
+                Lorem ipsum dolor sit amet .
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="user_info">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                <span>Adisdc md </span>
+                Lorem ipsum dolor sit amet .
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="user_info">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                <span>Adisdc md </span>
+                Lorem ipsum dolor sit amet .
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="user_info">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                <span>Adisdc md </span>
+                Lorem ipsum dolor sit amet .
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+
+        </div>
+        <div className="menu">
+          <span>Online Friends</span>
+          <div className="user">
+            <div className="user_info">
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <div className="online" />
+              <span>Lorem, ipsum.</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="user_info">
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <div className="online" />
+              <span>Lorem, ipsum.</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="user_info">
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <div className="online" />
+              <span>Lorem, ipsum.</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="user_info">
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <div className="online" />
+              <span>Lorem, ipsum.</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="user_info">
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <div className="online" />
+              <span>Lorem, ipsum.</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="user_info">
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <div className="online" />
+              <span>Lorem, ipsum.</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="user_info">
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <div className="online" />
+              <span>Lorem, ipsum.</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="user_info">
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <div className="online" />
+              <span>Lorem, ipsum.</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="user_info">
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <div className="online" />
+              <span>Lorem, ipsum.</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="user_info">
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <div className="online" />
+              <span>Lorem, ipsum.</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="user_info">
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <div className="online" />
+              <span>Lorem, ipsum.</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="user_info">
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <div className="online" />
+              <span>Lorem, ipsum.</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="user_info">
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <div className="online" />
+              <span>Lorem, ipsum.</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
