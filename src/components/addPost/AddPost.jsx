31d01ab0f70536962d762c8b1c9188a66a4f9cda
Elezaaby <mahmodalezaby@gmail.com>
2023-03-30 17:30:24 +0200

    add function add post

--- src/components/addPost/AddPost.jsx
+++ src/components/addPost/AddPost.jsx
@@ -1,14 +1,64 @@
-import React, { useContext } from 'react'
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useContext, useReducer, useRef, useEffect } from 'react'
 import './addPost.scss'
 import { AuthContext } from '../../context/authContext'
-
+import { PostsReducer, postActions, postsStates, } from "../../context/PostReducer";
 import Friends from "../../assets/friend.png";
 import AddImg from "../../assets/img.png";
 import Map from "../../assets/map.png";
+import { collection, doc, serverTimestamp, setDoc, query, onSnapshot } from 'firebase/firestore';
+import { db } from '../../firebase';
+import { orderBy } from 'firebase/firestore';
 
 
 const AddPost = () => {
-  const { userData } = useContext(AuthContext)
+  const { userData, user } = useContext(AuthContext)
+  const postRef = doc(collection(db, "posts"));
+  const collectionRef = collection(db, "posts");
+  const document = postRef.id;
+  const [state, dispatch] = useReducer(PostsReducer, postsStates);
+  const { SUBMIT_POST, HANDLE_ERROR } = postActions;
+  const inputText = useRef("");
+
+
+  const handleSubmitPost = async (e) => {
+    e.preventDefault();
+    if (inputText.current.value !== "") {
+      try {
+        await setDoc(postRef, {
+          documentId: document,
+          uid: user?.uid || userData?.uid,
+          profilePic: user?.photoURL || userData?.profileImg,
+          name: user?.displayName || userData?.name,
+          email: user?.email || userData?.email,
+          desc: inputText.current.value,
+          // image: image,
+          timestamp: serverTimestamp(),
+        });
+        inputText.current.value = "";
+      } catch (err) {
+        dispatch({ type: HANDLE_ERROR });
+        console.log(err.message);
+      }
+    } else {
+      dispatch({ type: HANDLE_ERROR });
+    }
+  };
+
+  useEffect(() => {
+    const postData = async () => {
+      const q = query(collectionRef, orderBy("timestamp", "asc"));
+      await onSnapshot(q, (doc) => {
+        dispatch({
+          type: SUBMIT_POST,
+          posts: doc?.docs?.map((item) => item?.data()),
+        });
+      });
+    };
+    return () => postData();
+  }, [SUBMIT_POST]);
+
 
   return (
     <div className='addPost'>
@@ -16,9 +66,9 @@ const AddPost = () => {
         <div className="user">
           <img src={userData.profileImg} alt="" />
         </div>
-        <div className="inputP">
-          <input type="text" placeholder={`What's on your mind ${userData.name}?`} />
-        </div>
+        <form onSubmit={handleSubmitPost} className="inputP">
+          <input ref={inputText} type="text" placeholder={`What's on your mind ${userData.name}?`} />
+        </form>
       </div>
       <div className="bottom">
         <div className="menu">
@@ -35,7 +85,7 @@ const AddPost = () => {
             <span>Tag Friends</span>
           </div>
         </div>
-        <button>Share</button>
+        <button onClick={handleSubmitPost}>Share</button>
       </div>
     </div>
   )
